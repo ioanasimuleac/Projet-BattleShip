02c7eaf3d3bcf142d45114c83e0c22e356390adb
AubinABADIE <aubin.abadie@gmail.com>
2018-05-23 16:25:44 +0200

    version finale

--- src/fr/battleship/TestIA.java
+++ src/fr/battleship/TestIA.java
@@ -6,7 +6,7 @@ import java.io.IOException;
 
 public class TestIA {
 
-	public static void main(String[] args) /*throws IOException*/ {
+	public static void main(String[] args){
 		
 		System.out.println("Premiere partie: BEGINNER VS MEDIUM");
 		PlayerAIMedium pm1 = new PlayerAIMedium();
